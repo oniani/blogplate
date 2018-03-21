580dffb0f57dac7e0a70fa0594de9605ae4dd4af
David Oniani <onianidavid@gmail.com>
2018-03-21 17:22:42 -0500

    Everything is validated and responsive.

--- js/typewriter.js
+++ js/typewriter.js
@@ -1,56 +1,25 @@
-var TxtType = function(el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 2000;
-    this.txt = '';
-    this.tick();
-    this.isDeleting = false;
-};
+var wrap = document.getElementsByClassName("typewriter_wrapper")[0],
+    txtEl = wrap.getElementsByTagName('span')[0],
+    txt = txtEl.innerHTML,
+    speed;
 
-    TxtType.prototype.tick = function() {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
+function typewriter() {
 
-    if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  // Delete old Text
+  txtEl.innerHTML = "";
+  
+  var count = 0,
+      len =  txt.length;
+  
+  var i = setInterval(function(){
+    txtEl.innerHTML = txt.substr(0, count);
+    count++;
+    if(count === len + 1) {
+      clearInterval(i);
     }
-
-    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
-    var that = this;
-    var delta = 200 - Math.random() * 100;
-
-    if (this.isDeleting) { delta /= 2; }
-
-    if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
-    this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
-    this.isDeleting = false;
-    this.loopNum++;
-    delta = 500;
-    }
-
-setTimeout(function() {
-that.tick();
-}, delta);
-};
-
-window.onload = function() {
-    var elements = document.getElementsByClassName('textwriter');
-    for (var i=0; i<elements.length; i++) {
-var toRotate = elements[i].getAttribute('data-type');
-var period = elements[i].getAttribute('data-period');
-if (toRotate) {
-new TxtType(elements[i], JSON.parse(toRotate), period);
+  }, 100) ;
 }
-    }
-    // ADD SOME CSS
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".textwriter > .wrap { border-right: 0.08em solid #fff}";
-    document.body.appendChild(css);
-};
+
+
+typewriter();
+
