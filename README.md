7dec04335a508f0232c1aa2a5f9a1b67cefedefe
David Oniani <onianidavid@gmail.com>
2019-09-10 17:09:49 -0500

    Prettier!

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Blog Template
+
 This is a fully costomizable multipurpose web template specially crafted for the personal blog. It is built using HTML5 and CSS3. The template utilizes CSS Grid layout and is fully responsive.
 
 **You can see the demo of the template by clicking [here](https://oniani.github.io/blogplate).**
 
 ## License
+
 [GNU General Public License v3.0](LICENSE)
--- js/typewriter.js
+++ js/typewriter.js
@@ -1,25 +1,22 @@
 var wrap = document.getElementsByClassName("typewriter_wrapper")[0],
-    txtEl = wrap.getElementsByTagName('span')[0],
-    txt = txtEl.innerHTML,
-    speed;
+  txtEl = wrap.getElementsByTagName("span")[0],
+  txt = txtEl.innerHTML,
+  speed;
 
 function typewriter() {
-
   // Delete old Text
   txtEl.innerHTML = "";
-  
+
   var count = 0,
-      len =  txt.length;
-  
-  var i = setInterval(function(){
+    len = txt.length;
+
+  var i = setInterval(function() {
     txtEl.innerHTML = txt.substr(0, count);
     count++;
-    if(count === len + 1) {
+    if (count === len + 1) {
       clearInterval(i);
     }
-  }, 100) ;
+  }, 100);
 }
 
-
 typewriter();
-
